f5d6388de8dea6cf1763bcb098c59651ee927f2b
bcampeau <brad@alleyinteractive.com>
2015-07-20 14:46:29 -0400

    Fixed singleton instantiation, moved base plugin functions to new class

--- ad-layers.php
+++ ad-layers.php
@@ -65,6 +65,11 @@ define( 'AD_LAYERS_OPTION_NAME', 'ad_layers_settings' );
 require_once( AD_LAYERS_BASE_DIR . '/php/class-ad-layers-singleton.php' );
 
 /**
+ * Base plugin functionality and global scripts.
+ */
+require_once( AD_LAYERS_BASE_DIR . '/php/class-ad-layers.php' );
+
+/**
  * Custom post type.
  */
 require_once( AD_LAYERS_BASE_DIR . '/php/class-ad-layers-post-type.php' );
@@ -87,70 +92,3 @@ if ( is_admin() ) {
 	require_once( AD_LAYERS_BASE_DIR . '/php/class-ad-layers-admin.php' );
 }
 
-/**
- * Instantiate the Ad Layers base class to handle required plugin setup
- */
-if ( ! class_exists( 'Ad_Layers' ) ) :
-
-class Ad_Layers extends Ad_Layers_Singleton {
-	
-	/**
-	 * Built-in ad server support
-	 *
-	 * @var Ad_Layers
-	 */
-	public $ad_servers;
-	
-	/**
-	 * Current ad layers settings
-	 *
-	 * @var string
-	 */
-	public $settings;
-	
-	/**
-	 * Setup the singleton.
-	 */
-	public function setup() {
-		// Load current settings
-		$this->settings = get_option( AD_LAYERS_OPTION_NAME );
-	
-		// Allow additional ad servers to be loaded via filter within a theme
-		$this->ad_servers = apply_filters( 'ad_layers_ad_servers', array(
-			'DFP' => AD_LAYERS_BASE_DIR . '/php/ad-servers/class-dfp.php',
-		) );
-		
-		if ( ! empty( $this->ad_servers ) && is_array( $this->ad_servers ) ) {
-			foreach ( $this->ad_servers as $ad_server ) {
-				if ( file_exists( $ad_server ) ) {
-					require_once( $ad_server );
-				}
-			}
-		}
-		
-		// Load the base Javascript library early
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 5 );
-	}
-	
-	/**
-	 * Load scripts.
-	 */
-	public function enqueue_scripts() {
-		// Load the base Javascript library
-		wp_enqueue_script( 'ad-layers-js', AD_LAYERS_BASE_DIR . '/js/ad-layers.js', array( 'jquery' ), AD_LAYERS_GLOBAL_ASSET_VERSION, false );
-		
-		// If set, localize with the active ad server
-		if ( ! empty( $this->settings['ad_server'] ) ) {
-			wp_localize_script( 'ad-layers-js', 'ad-layers', array(
-				'ad_server' => $this->settings['ad_server'],
-			) );
-		}
-		
-		// Load the CSS. Mostly used in debug mode.
-		wp_enqueue_style( 'ad-layers-css', AD_LAYERS_BASE_DIR . '/css/ad-layers.css', array(), AD_LAYERS_GLOBAL_ASSET_VERSION );
-	}
-}
-
-Ad_Layers::instance();
-
-endif;
