c35a1f58c1a47ab843a1b35cf8b932e23c5e6586
bcampeau <brad@alleyinteractive.com>
2015-09-03 16:39:51 -0400

    Excluding ad layers from search

--- php/class-ad-layers-post-type.php
+++ php/class-ad-layers-post-type.php
@@ -25,19 +25,19 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 	public function setup() {
 		// Create the post type
 		add_action( 'init', array( $this, 'create_post_type' ) );
-		
+
 		// Add the custom meta boxes for managing this post type
 		add_action( 'fm_post_' . $this->post_type, array( $this, 'add_meta_boxes' ) );
-		
+
 		// Add custom columns for the list table
 		add_filter( 'manage_' . $this->post_type . '_posts_columns' , array( $this, 'manage_edit_columns' ), 15, 1 );
 		add_action( 'manage_' . $this->post_type . '_posts_custom_column' , array( $this, 'manage_custom_columns' ), 10, 2 );
-		
+
 		// Add and remove data from the options list of available ad layers
 		add_action( 'save_post_' . $this->post_type, array( $this, 'save_post' ), 99, 3 );
 		add_action( 'delete_post', array( $this, 'delete_post' ) );
 	}
-	
+
 	/**
 	 * Creates the post type.
 	 */
@@ -59,6 +59,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 			'menu_icon' => 'dashicons-schedule',
 			'public' => true,
 			'publicly_queryable' => false,
+			'exclude_from_search' => true,
 			'show_in_menu' => true,
 			'show_in_nav_menus' => false,
 			'supports' => array( 'title', 'revisions' ),
@@ -80,14 +81,14 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
   		$columns['ad_layer_taxonomies'] = __( 'Taxonomies', 'ad-layers' );
   		$columns['ad_layer_ad_units'] = __( 'Ad Units', 'ad-layers' );
   		$columns['ad_layer_priority'] = __( 'Priority', 'ad-layers' );
-  		
+
   		// Move date back to the end
   		unset( $columns['date'] );
   		$columns['date'] = __( 'Date', 'ad-layers' );
-  		
+
   		return apply_filters( 'ad_layers_edit_columns', $columns );
 	}
-	
+
 	/**
 	 * Manage custom column values.
 	 *
@@ -119,7 +120,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 				break;
 		}
 	}
-	
+
 	/**
 	 * Adds the meta boxes required to manage an ad layer.
 	 *
@@ -131,7 +132,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 		if ( ! class_exists( 'Fieldmanager_Field' ) ) {
 			return;
 		}
-	
+
 		// Add ad units
 		$ad_unit_args = array(
 			'name' => 'ad_layer_ad_units',
@@ -150,15 +151,15 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 				),
 			)
 		);
-		
+
 		$targeting_args = Ad_Layers_Ad_Server::instance()->get_custom_targeting_args( 'custom_targeting' );
 		if ( ! empty( $targeting_args ) ) {
 			$ad_unit_args['children']['custom_targeting'] = new Fieldmanager_Group( apply_filters( 'ad_layers_custom_targeting_ad_unit_args', $targeting_args ) );
 		}
-		
+
 		$fm_ad_units = new Fieldmanager_Group( apply_filters( 'ad_layers_ad_units_field_args', $ad_unit_args ) );
 		$fm_ad_units->add_meta_box( __( 'Ad Units', 'ad-layers' ), $this->post_type, 'normal', 'high' );
-		
+
 		// Add page types
 		$fm_page_types = new Fieldmanager_Select(
 			apply_filters( 'ad_layers_page_types_field_args', array(
@@ -172,7 +173,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 			) )
 		);
 		$fm_page_types->add_meta_box( __( 'Page Types', 'ad-layers' ), $this->post_type, 'normal', 'high' );
-		
+
 		// Add taxonomies
 		$fm_taxonomies = new Fieldmanager_Select(
 			apply_filters( 'ad_layers_taxonomies_field_args', array(
@@ -186,7 +187,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 			) )
 		);
 		$fm_taxonomies->add_meta_box( __( 'Taxonomies', 'ad-layers' ), $this->post_type, 'normal', 'high' );
-		
+
 		// Add post types
 		$fm_post_types = new Fieldmanager_Select(
 			apply_filters( 'ad_layers_post_types_field_args', array(
@@ -200,15 +201,15 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 			) )
 		);
 		$fm_post_types->add_meta_box( __( 'Post Types', 'ad-layers' ), $this->post_type, 'normal', 'high' );
-		
+
 		// Custom targeting variables
 		$targeting_args = Ad_Layers_Ad_Server::instance()->get_custom_targeting_args();
 		if ( ! empty( $targeting_args ) ) {
 			$fm_custom = new Fieldmanager_Group( apply_filters( 'ad_layers_custom_targeting_field_args', $targeting_args ) );
-			$fm_custom->add_meta_box( __( 'Page Level Custom Targeting', 'ad-layers' ), $this->post_type, 'normal', 'low' );	
+			$fm_custom->add_meta_box( __( 'Page Level Custom Targeting', 'ad-layers' ), $this->post_type, 'normal', 'low' );
 		}
 	}
-	
+
 	/**
 	 * Gets the currently saved taxonomies for this post.
 	 *
@@ -219,10 +220,10 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 		if ( ! isset( $_GET['post'] ) ) {
 			return array();
 		}
-		
+
 		return get_post_meta( intval( $_GET['post'] ), 'ad_layer_taxonomies', true );
 	}
-	
+
 	/**
 	 * Gets the post type name used for ad layers.
 	 *
@@ -232,7 +233,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 	public function get_post_type() {
 		return $this->post_type;
 	}
-	
+
 	/**
 	 * Decide how to manage this post in the ad layer list on save.
 	 *
@@ -245,16 +246,16 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 		if ( 'auto-draft' == $post->post_status ) {
 			return;
 		}
-	
+
 		// Get the current global list
 		$ad_layers = get_option( 'ad_layers', array() );
-		
+
 		// Create the data to be saved
 		$new_layer = array(
 			'post_id' => $post_id,
 			'title' => $post->post_title,
 		);
-		
+
 		// If this is not an update, just append it.
 		// Otherwise, find and update the layer.
 		$position = null;
@@ -264,7 +265,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 				$this->delete_post( $post_id );
 				return;
 			}
-			
+
 			// Otherwise, find and update the layer.
 			foreach ( $ad_layers as $i => $layer ) {
 				if ( $layer['post_id'] == $post_id ) {
@@ -273,16 +274,16 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 				}
 			}
 		}
-		
+
 		if ( null === $position ) {
 			$position = count( $ad_layers );
 		}
-	
+
 		$ad_layers[ $position ] = $new_layer;
-		
+
 		update_option( 'ad_layers', apply_filters( 'ad_layers_save_post', $ad_layers ) );
 	}
-	
+
 	/**
 	 * Remove this post from the ad layer list on delete.
 	 *
@@ -292,7 +293,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 	public function delete_post( $post_id ) {
 		// Get the current global list
 		$ad_layers = get_option( 'ad_layers' );
-		
+
 		// Find and remove the layer
 		foreach ( $ad_layers as $i => $layer ) {
 			if ( $layer['post_id'] == $post_id ) {
@@ -300,7 +301,7 @@ class Ad_Layers_Post_Type extends Ad_Layers_Singleton {
 				break;
 			}
 		}
-		
+
 		update_option( 'ad_layers', apply_filters( 'ad_layers_delete_post', $ad_layers ) );
 	}
 }
