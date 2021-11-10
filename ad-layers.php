2830767a6079e4426bfa0629a66555696e0b3d4c
jomurgel <dev@jomurgel.com>
2021-11-10 08:53:46 -0700

    loads admin elements after theme setup

--- ad-layers.php
+++ ad-layers.php
@@ -26,6 +26,8 @@ if ( ! defined( 'AD_LAYERS_OPTION_NAME' ) ) {
 
 // Include functions for working with assets (primarily JavaScript).
 require_once __DIR__ . '/inc/assets.php';
+
+// TODO: Resolve conflict with other plugins.
 require_once __DIR__ . '/inc/asset-loader-bridge.php';
 
 // Include functions for working with meta.
@@ -34,8 +36,15 @@ require_once __DIR__ . '/inc/meta.php';
 // Include functions.php for registering custom post types, etc.
 require_once __DIR__ . '/functions.php';
 
-// Include admin-facing features.
-if ( is_admin() ) {
-	require_once __DIR__ . '/inc/class-ad-layers-admin.php';
-	require_once __DIR__ . '/inc/class-ad-layers-meta-boxes.php';
+/**
+ * Load assets after theme setup.
+ *
+ * @return void
+ */
+function load_after_theme_setup() {
+	if ( is_admin() ) {
+		require_once __DIR__ . '/inc/class-ad-layers-admin.php';
+		require_once __DIR__ . '/inc/class-ad-layers-meta-boxes.php';
+	}
 }
+add_action( 'after_theme_setup', __NAMESPACE__ . '\load_after_theme_setup' );
