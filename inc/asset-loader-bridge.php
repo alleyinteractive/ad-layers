4f0245dfa901bcc3c1e0dd1c18d7bf2177414c80
jomurgel <dev@jomurgel.com>
2021-12-22 07:49:40 -0700

    repairs missing ad layers functions

--- inc/asset-loader-bridge.php
+++ inc/asset-loader-bridge.php
@@ -12,60 +12,52 @@
 
 /* phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound */
 
-if ( ! function_exists( 'Ad_Layers\get_ad_layers_dependencies' ) ) {
-	/**
-	 * A helper function to bridge the gap between the non-namespaced scaffolded
-	 * block files and the namespaced block loader in the plugin. Calls
-	 * get_ad_layers_dependencies.
-	 *
-	 * @param string $asset Entry point and asset type separated by a '.'.
-	 *
-	 * @return array The asset dependencies.
-	 */
-	function get_ad_layers_dependencies( $asset ) {
-		return Ad_Layers\get_ad_layers_dependencies( $asset );
-	}
+/**
+ * A helper function to bridge the gap between the non-namespaced scaffolded
+ * block files and the namespaced block loader in the plugin. Calls
+ * get_ad_layers_dependencies.
+ *
+ * @param string $asset Entry point and asset type separated by a '.'.
+ *
+ * @return array The asset dependencies.
+ */
+function get_ad_layers_dependencies( $asset ) {
+	return Ad_Layers\get_ad_layers_dependencies( $asset );
 }
 
-if ( ! function_exists( 'Ad_Layers\get_ad_layers_hash' ) ) {
-	/**
-	 * A helper function to bridge the gap between the non-namespaced scaffolded
-	 * block files and the namespaced block loader in the plugin. Calls
-	 * get_ad_layers_hash.
-	 *
-	 * @param string $asset Entry point and asset type separated by a '.'.
-	 *
-	 * @return string The asset version hash.
-	 */
-	function get_ad_layers_hash( $asset ) {
-		return Ad_Layers\get_ad_layers_hash( $asset );
-	}
+/**
+ * A helper function to bridge the gap between the non-namespaced scaffolded
+ * block files and the namespaced block loader in the plugin. Calls
+ * get_ad_layers_hash.
+ *
+ * @param string $asset Entry point and asset type separated by a '.'.
+ *
+ * @return string The asset version hash.
+ */
+function get_ad_layers_hash( $asset ) {
+	return Ad_Layers\get_ad_layers_hash( $asset );
 }
 
-if ( ! function_exists( 'Ad_Layers\get_ad_layers_path' ) ) {
-	/**
-	 * A helper function to bridge the gap between the non-namespaced scaffolded
-	 * block files and the namespaced block loader in the plugin. Calls
-	 * get_ad_layers_hash.
-	 *
-	 * @param string  $asset Entry point and asset type separated by a '.'.
-	 * @param boolean $dir   Optional. Whether to return the directory path or the plugin URL path. Defaults to false (returns URL).
-	 *
-	 * @return string The asset URL.
-	 */
-	function get_ad_layers_path( $asset, $dir = false ) {
-		return Ad_Layers\get_ad_layers_path( $asset, $dir );
-	}
+/**
+ * A helper function to bridge the gap between the non-namespaced scaffolded
+ * block files and the namespaced block loader in the plugin. Calls
+ * get_ad_layers_hash.
+ *
+ * @param string  $asset Entry point and asset type separated by a '.'.
+ * @param boolean $dir   Optional. Whether to return the directory path or the plugin URL path. Defaults to false (returns URL).
+ *
+ * @return string The asset URL.
+ */
+function get_ad_layers_path( $asset, $dir = false ) {
+	return Ad_Layers\get_ad_layers_path( $asset, $dir );
 }
 
-if ( ! function_exists( 'Ad_Layers\inline_ad_layers_locale_data' ) ) {
-	/**
-	 * A helper function to bridge the gap between the non-namespaced scaffolded
-	 * block files and the namespaced inline locale function in the plugin.
-	 *
-	 * @param string $to_handle The script handle to attach the inline script to.
-	 */
-	function inline_ad_layers_locale_data( $to_handle ) {
-		Ad_Layers\inline_ad_layers_locale_data( $to_handle );
-	}
+/**
+ * A helper function to bridge the gap between the non-namespaced scaffolded
+ * block files and the namespaced inline locale function in the plugin.
+ *
+ * @param string $to_handle The script handle to attach the inline script to.
+ */
+function inline_ad_layers_locale_data( $to_handle ) {
+	Ad_Layers\inline_ad_layers_locale_data( $to_handle );
 }
